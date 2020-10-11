fcd8e07a92abe201bfdea9d639fd6bdc6050adfb
ToMattBan <souaquelecleyton@gmail.com>
2020-10-11 12:58:29 -0300

    Números primos e botão de voltar para 0

--- 4o_Sem/Flutter/first/lib/main.dart
+++ 4o_Sem/Flutter/first/lib/main.dart
@@ -13,7 +13,8 @@ class MyApp extends StatelessWidget {
           primarySwatch: Colors.blue,
           scaffoldBackgroundColor: Colors.black,
           visualDensity: VisualDensity.adaptivePlatformDensity,
-          textTheme: TextTheme(body1: TextStyle(color: Colors.white))),
+          textTheme: TextTheme(
+              bodyText2: TextStyle(color: Colors.white, height: 1.5))),
       home: MyHomePage(title: 'El Grandioso Countador e Parificador'),
     );
   }
@@ -29,54 +30,115 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  var _counter = 0;
   var _color = Colors.white;
-  String _pi = "0 não é par nem ímpar";
+  var _pi = "0 não é par nem ímpar";
 
   void _incrementCounter() {
     setState(() {
       _counter++;
-      _pi = _counter % 2 == 0 ? "PAR" : "ÍMPAR";
-      _color = _pi == "PAR" ? Colors.blue : Colors.red;
     });
+    _attValues();
+  }
+
+  void _decreaseCounter() {
+    setState(() {
+      _counter--;
+    });
+    _attValues();
+  }
+
+  void _restartCounter() {
+    setState(() {
+      _counter = 0;
+    });
+    _attValues();
+  }
+
+  void _attValues() {
+    void _parImpar() {
+      _pi = _counter == 0
+          ? "0 não é par nem ímpar"
+          : _counter % 2 == 0
+              ? "PAR"
+              : "ÍMPAR";
+    }
+
+    _parImpar();
+
+    if (_counter == 2) _pi = "PRIMO";
+
+    for (var i = 2; i < _counter; i++) {
+      if (_counter % i == 0) {
+        _parImpar();
+        break;
+      } else {
+        _pi = _counter == 0 ? "0 não é par nem ímpar" : "PRIMO";
+      }
+    }
+
+    _color = _counter == 0
+        ? Colors.white
+        : _pi == "PAR"
+            ? Colors.blue
+            : _pi == "PRIMO"
+                ? Colors.purple
+                : Colors.red;
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Nusha, sabe quantas vezes tu clicou no botão lá? Olha ae:',
-              textAlign: TextAlign.center,
-              style: TextStyle(height: 1.5),
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                'Nusha, sabe quantas vezes tu clicou no botão lá? Olha ae:',
+                textAlign: TextAlign.center,
+                style: TextStyle(height: 1.5),
+              ),
+              Text(
+                '$_counter',
+                style: TextStyle(fontSize: 38),
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              Text(
+                _counter == 0 ? '$_pi' : 'Rapash, teu número é:',
+                textAlign: TextAlign.center,
+                style: TextStyle(color: _color, height: 1.5),
+              ),
+              Text(
+                _counter == 0 ? '' : '$_pi',
+                style: TextStyle(color: _color, fontSize: 34),
+              ),
+            ],
+          ),
+        ),
+        floatingActionButton: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            FloatingActionButton(
+              onPressed: _decreaseCounter,
+              tooltip: 'Descontar',
+              child: Icon(Icons.remove),
             ),
-            Text(
-              '$_counter',
-              style: TextStyle(fontSize: 38),
+            FloatingActionButton(
+              onPressed: _restartCounter,
+              tooltip: 'Reiniciar',
+              child: Icon(Icons.refresh),
             ),
-            Text(
-              _counter == 0 ? '$_pi' : 'Rapash, teu número é:',
-              textAlign: TextAlign.center,
-              style: TextStyle(color: _color, height: 1.5),
-            ),
-            Text(
-              _counter == 0 ? '' : '$_pi',
-              style: TextStyle(color: _color, fontSize: 34),
+            FloatingActionButton(
+              onPressed: _incrementCounter,
+              tooltip: 'Aumentar',
+              child: Icon(Icons.add),
             ),
           ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Clicador',
-        child: Icon(Icons.add),
-      ),
-    );
+        ));
   }
 }
